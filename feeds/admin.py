e0f342d41e9dcd9f9cbabbce116699b8b6098542
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-12-05 10:53:51 +0800

    Enabled Feed admin page to display different forms for adding and changing records

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin, messages
 from django.http import HttpResponseRedirect
 
+from feeds.forms import FeedAdminAddForm, FeedAdminChangeForm
 from feeds.models import Feed
 
 
 @admin.register(Feed)
 class FeedAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'version', 'link']
-    fields = ['link', 'title', 'description']
+    form = FeedAdminAddForm
 
     def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
         """Ensures errors are handled when submitting forms in admin.
@@ -21,3 +22,9 @@ class FeedAdmin(admin.ModelAdmin):
             self.message_user(request, e, level=messages.ERROR)
             return HttpResponseRedirect(form_url)
         return response
+
+    def get_form(self, request, obj=None, **kwargs):
+        if obj:
+            return FeedAdminChangeForm
+        else:
+            return super().get_form(request, obj, **kwargs)
