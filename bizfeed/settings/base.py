baf4a87fbbf075ec612510f1b2a3e7dc55ec2fea
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-12-08 17:32:25 +0800

    Installed celery and updated project settings

--- bizfeed/settings/base.py
+++ bizfeed/settings/base.py
@@ -124,3 +124,17 @@ STATIC_URL = '/static/'
 # Count of consecutive existing entries before stopping parsing process
 # This ensures entries already saved in db are not parsed again
 MAX_SAVED_ENTRIES_COUNT = 3
+
+# Celery settings
+CELERY_BROKER_URL = os.environ.get(
+    'CELERY_BROKER_URL',
+    'redis://localhost:6379'
+)
+CELERY_RESULT_BACKEND = os.environ.get(
+    'CELERY_RESULT_BACKEND',
+    'redis://localhost:6379'
+)
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
